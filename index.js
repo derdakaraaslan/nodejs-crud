bcc7de9dc2e77449f3e65c9a282a8f5dd7802932
Abdurrahman Derda Karaaslan <derdakaraaslan02@gmail.com>
2024-10-25 23:50:32 +0300

    nodemon added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Hello World!, this is a test');
+}); 
