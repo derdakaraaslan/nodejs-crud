565e83e05cb424224ba56096bf26966991746901
Abdurrahman Derda Karaaslan <derdakaraaslan02@gmail.com>
2024-10-26 00:06:32 +0300

    mongo db connection added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
@@ -7,4 +12,13 @@ app.listen(3000, () => {
 
 app.get('/test', (req, res) => {
     res.send('Hello World!, this is a test');
-}); 
+});
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(
+    () => {
+        console.log('Connected to database!');
+    }
+).catch((err) => {
+    console.log('Connection failed!', err);
+});
